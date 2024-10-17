ecda72daf8c84c0a6f70a89090df5779a7d1ee2b
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-10-18 11:54:46 +1300

    add styling to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
-import PlusButton from "./components/PlusButton.tsx";
+import React, { useState } from 'react'
+import PlusButton from './components/PlusButton.tsx'
+import RowEntry from './components/RowEntry.tsx'
 
 function App() {
-  const [entires, setEntries] = useState([]);
+  // HELLO
+  const [entires, setEntries] = useState([])
 
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false)
 
   const handleClick = () => {
-    setActive(true);
-  };
+    setActive(true)
+  }
 
   return (
-    <div className="min-h-dvh">
+    <div className="min-h-dvh flex flex-col justify-start items-center container mx-auto py-12">
+      <RowEntry name={'Flat White'} cost={'5.50'} />
       {entires.map((item) => item)}
-      {active === true ? "Hello World" : ""}
+      {active === true ? 'Hello World' : ''}
       <PlusButton
         onClick={handleClick}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
