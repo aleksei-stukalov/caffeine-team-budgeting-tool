9f85fb92fb7b42a4bd4abd40fab661d29e1c99a0
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-10-18 10:16:18 +1300

    plus button render 'hello world'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
 import PlusButton  from "./components/PlusButton.tsx"
 
+
 function App() {
+
+  const [entires, setEntries] = useState([]); 
+  
+  const [active, setActive] = useState(false); 
+
+  const handleClick = () => {
+
+   setActive(true)
+  }
+
   return <div className="min-h-dvh">
-    <PlusButton />
+    {entires.map(item => item)}
+    {active === true ? "Hello World" : ""}
+    <PlusButton onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"/>
+
   </div>
 }
 
--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -1,13 +1,24 @@
-const handleClick = () => {}
+import React, { useState } from 'react';
 
-export default function PlusButton() {
+const _placeHolder = () => {
 
-    return (
-
-        <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">+</button>
-    )
-
-
+    return (<div>Hello World</div>)
 }
 
+export default function PlusButton(props) {
 
+
+    const handleClick = () => {
+        setPlaceHolder(_placeHolder);
+        
+    };
+
+    return (
+        <button
+            {...props}
+           // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+            +
+        </button>
+    );
+}
