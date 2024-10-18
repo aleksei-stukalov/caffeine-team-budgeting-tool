1f530095a8e0ab88eb605b2be8b0a7b1afbea663
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-10-18 14:06:06 +1300

    idk

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -1,4 +1,7 @@
-export default function PlusButton(props) {
+import { ButtonHTMLAttributes } from "react";
+
+
+export default function PlusButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-4 px-4 rounded-md"
