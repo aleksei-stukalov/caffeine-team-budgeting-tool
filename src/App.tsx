93cb2ec582cc0130edd060c7e8ffeb4aa96de27c
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-10-18 13:29:11 +1300

    style app and add vertical alignment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import PlusButton from './components/PlusButton.tsx'
 import InputField from './components/InputField.tsx'
 import RowEntry from './components/RowEntry.tsx'
+import DisplayTotal from './components/DisplayTotal.tsx'
 
 interface Entry {
   id: number
@@ -25,9 +26,13 @@ function App() {
   }
 
   return (
-    <div className="min-h-dvh flex flex-col justify-start items-center container mx-auto py-12">
+    <div className="min-h-dvh flex flex-col justify-start items-center w-1/2 mx-auto py-12">
+      <p className="text-2xl font-semibold mb-8">Coffee Expenses calculator</p>
       {entries.map((item) => (
-        <RowEntry key={item.id} name={item.name} cost={item.cost} />
+        <>
+          <RowEntry key={item.id} name={item.name} cost={item.cost} />
+          <DisplayTotal expenses={entries} />
+        </>
       ))}
       {active === true ? (
         <InputField addExpense={addExpense} />
@@ -35,7 +40,7 @@ function App() {
         <PlusButton
           main
           onClick={handleClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-8 rounded-full"
         />
       )}
     </div>
